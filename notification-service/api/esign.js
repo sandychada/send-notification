6eb9598774e7b30ddbe87597893e05bb7a89537d
Vamsi Chada <vamsikrishnachada@gmail.com>
2021-05-14 21:20:34 -0500

    removing creds

--- notification-service/api/esign.js
+++ notification-service/api/esign.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const accountSid = 'AC4fecc83fda6cb66926627cb17976717c';
-const authToken = '2e39783a652e8a5dc04a6db2fdeb7875';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 //creating and using tables in dynamodb
